fd22cd22550ad76caca6291a6bb86fbeede9c430
Komari Herring <komherr@gmail.com>
2016-09-05 22:12:32 -0400

    Initial Commit

---
+++ HighVoltage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighVoltage
+//
+//  Created by Komari Herring on 9/5/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
